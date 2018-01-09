3a88296c73c8fc0f141be9f13c1db458e03a64a2
tucker.bane@icloud.com <Tucker.bane@icloud.com>
2018-01-08 18:35:36 -0800

    Unit list start

---
+++ Assets/ListRegisterer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListRegisterer : MonoBehaviour {
+
+    // pick one of these and go through the meta data to find a list to add yourself to
+    public MonoBehaviour mRegistrationTarget;
+    public GameObject mObjectRegistrationTarget;
+
+    // Use this for initialization
+    void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
